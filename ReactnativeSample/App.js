c3c5e971ea73cf43de1ad3ab5c5254155b6aa50c
Matin <m.zareimatin@gmail.com>
2021-07-24 16:47:30 +0430

    update react native sample application

--- ReactnativeSample/App.js
+++ ReactnativeSample/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import AppContainer from './src/navigation'
+import { MetrixDeeplink } from "@metrixorg/deeplink-react-native-plugin"
 
 class App extends React.Component {
 
   constructor() {
       super();
+      MetrixDeeplink.setDeferredDeeplinkResponseListener(this.deeplinkCallback);
   }
 
   render() {
     return <AppContainer />      
   }
+
+  deeplinkCallback = (type, target, data) => {
+    console.log("[ METRIX_DEEPLINK_SAMPLE ]: Deferred deeplink callback called. Type: " + type + ". Target: " + target + ". Data: " + JSON.stringify(data));
+  };
 }
 
 export default App
