3f49c69d786a454e8acd9c813b102e3998d8ea0f
Matin <m.zareimatin@gmail.com>
2021-07-24 16:21:51 +0430

    implement react native deferred deeplink callback  api

--- Reactnative/android/src/main/java/ir/metrix/deeplink/reactnative/MetrixDeeplinkModule.java
+++ Reactnative/android/src/main/java/ir/metrix/deeplink/reactnative/MetrixDeeplinkModule.java
@@ -2,6 +2,18 @@ package ir.metrix.deeplink.reactnative;
 
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.Map;
+
+import org.jetbrains.annotations.NotNull;
+
+import ir.metrix.deeplink.OnMetrixDeferredDeeplinkListener;
+import ir.metrix.deeplink.MetrixDeeplinkModel;
+import ir.metrix.deeplink.MetrixDeeplink;
 
 public class MetrixDeeplinkModule extends ReactContextBaseJavaModule {
 
@@ -14,5 +26,29 @@ public class MetrixDeeplinkModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return TAG;
+    }
+
+    @ReactMethod
+    public void setDeferredDeeplinkResponseListener(final boolean shouldLaunchDeeplink, final Callback callback) {
+
+        if (callback != null) {
+            MetrixDeeplink.setDeferredDeeplinkResponseListener(new OnMetrixDeferredDeeplinkListener() {
+                @Override
+                public boolean launchReceivedDeeplink(@NotNull MetrixDeeplinkModel metrixDeeplinkModel) {
+                    callback.invoke(metrixDeeplinkModel.getType().name(), metrixDeeplinkModel.getTarget(), getWritableMapFromData(metrixDeeplinkModel.getData()));
+                    return shouldLaunchDeeplink;
+                }
+            });
+        }
+    }
+
+    private WritableMap getWritableMapFromData(Map<String, String> data) {
+        WritableMap map = Arguments.createMap();
+        
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            map.putString(entry.getKey(), entry.getValue());
+        }
+
+        return map;
     }
 }
--- Reactnative/index.js
+++ Reactnative/index.js
@@ -6,4 +6,10 @@ let metrixDeeplinkModule = NativeModules.MetrixDeeplink;
 
 var MetrixDeeplink = {};
 
+MetrixDeeplink.shouldLaunchDeeplink = true
+
+MetrixDeeplink.setDeferredDeeplinkResponseListener = function(callback) {
+    metrixDeeplinkModule.setDeferredDeeplinkResponseListener(this.shouldLaunchDeeplink, callback);
+};
+
 module.exports = { MetrixDeeplink }
